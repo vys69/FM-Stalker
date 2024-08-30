fd6346a2dd51870330ba48e360c1d9cf3be20be6
grim <75869731+vys69@users.noreply.github.com>
2024-08-30 01:01:48 -0700

    yuh yuh yuh yuh yuh

--- src/components/NowPlaying.js
+++ src/components/NowPlaying.js
@@ -8,19 +8,30 @@ const NowPlaying = ({ currentTrack, error, onRefresh, isListening }) => {
   }
 
   if (!isListening) {
-    return <div>{DEFAULT_USERNAME} isn't listening to music right now</div>;
+    return (
+      <div>
+        <p>{DEFAULT_USERNAME} isn't listening to music right now</p>
+        <button onClick={onRefresh}>Refresh</button>
+      </div>
+    );
   }
 
   if (!currentTrack) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <p>Loading...</p>
+        <button onClick={onRefresh}>Refresh</button>
+      </div>
+    );
   }
 
   const albumArt = currentTrack.image && currentTrack.image.length > 2 ? currentTrack.image[2]['#text'] : null;
 
   return (
     <div>
-      {albumArt && <img src={albumArt} alt="Album Art" draggable="false" style={{ width: '100%', maxWidth: '300px' }} />}
-      <p>{currentTrack.name} by {currentTrack.artist['#text']}</p>
+      {albumArt && <img src={albumArt} alt="Album Art" style={{ width: '100%', maxWidth: '300px' }} />}
+      <p><strong>{currentTrack.name}</strong></p>
+      <p>{currentTrack.artist['#text']}</p>
       <button onClick={onRefresh}>Refresh</button>
     </div>
   );
